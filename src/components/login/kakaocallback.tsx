41d641a0316ead79ac85d2a1ebdfc76758e40c90
kivccc <71814049+kivccc@users.noreply.github.com>
2024-10-23 03:59:03 +0900

    feat : 카카오 인가 코드 처리

--- src/components/login/kakaocallback.tsx
+++ src/components/login/kakaocallback.tsx
@@ -1,40 +1,37 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-
+import axios from "axios";
 interface ApiResponse {
   result: {
     user_id: string;
     jwt: string;
   };
 }
-
 export function KakaoRedirect() {
   const navigate = useNavigate();
   const code: string | null = new URL(window.location.href).searchParams.get("code");
 
   console.log(code);
 
-  const headers: HeadersInit = {
-    "Content-Type": "application/x-www-form-urlencoded",
-  };
+
 
   useEffect(() => {
-    fetch(`보내줄 주소?code=${code}`, {
-      method: "POST",
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((data: ApiResponse) => {
-        console.log(data);
-        console.log(data.result.user_id);
-        console.log(data.result.jwt);
-      })
-      .catch((error) => {
-        console.error("오류 발생", error);
-      });
+    const kakaologin= async()=>{
+      try{
+          
+          const res = await axios.get(`/oauth2/oauth2/callback/${code}`);
+          console.log(res);
+      }
+      catch(err){
+        console.error(err);
+      }
+    };
+    kakaologin();
+    
   }, [code]);
 
   return <div>로그인 중입니다.</div>;
 }
+
 
 export default KakaoRedirect;
